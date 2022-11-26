695fc05b73d791793004b57f14169f7afdc9af57
nasar06 <nasar5847@gmail.com>
2022-11-26 22:43:20 +0600

    setup vercel

---
+++ index.js
@@ -0,0 +1,210 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config();
+const jwt = require('jsonwebtoken')
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+
+// connect to database
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@assignment12.nrcbtlb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+function verifyJwt(req, res, next){
+    const authHeader = req.headers.authorization;
+    if(!authHeader){
+        return res.status(401).send('unauthorize access-->')
+    }
+
+    const token = authHeader.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, function(err, decoded){
+        if(err){
+            return res.status(403).send('forbidden access-->')
+        }
+        req.decoded = decoded
+        next()
+    })
+}
+
+function run(){
+    try{
+        
+        //database collections
+        const categoriesCollection = client.db('Clear-Pixel').collection('categories')
+        const camerasCollection = client.db('Clear-Pixel').collection('cameras')
+        const OrdersCollection = client.db('Clear-Pixel').collection('Orders')
+        const usersCollection = client.db('Clear-Pixel').collection('users')
+
+
+        //get all categories [home page]
+        app.get('/categories', async(req, res)=>{
+            const query = {}
+            const result = await categoriesCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        // get category to cameras [category page]
+        app.get('/category/:id', async(req, res)=>{
+            const id= req.params.id
+            const query = {categoryId: id}
+            const result = await camerasCollection.find(query).toArray();
+            res.send(result)
+        })
+
+        //post Order [Order modal page]
+        app.post('/orders', async(req, res)=>{
+            const order = req.body;
+            const result = await OrdersCollection.insertOne(order)
+            res.send(result)
+        })
+
+        //post users [signUp]
+        app.post('/users', async(req, res)=>{
+            const user = req.body;
+            const result = await usersCollection.insertOne(user)
+            res.send(result)
+        })
+
+        //get Orders (bayer) [my orders page]
+        app.get('/myOrders',verifyJwt, async(req, res)=>{
+            const email = req.query.email
+            const query = {userEmail: email}
+            const result = await OrdersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        //delete order
+        app.delete('/order/:id', async(req, res)=>{
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)}
+            const result = await OrdersCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        //post product [add product page]
+        app.post('/addProduct',async(req, res)=>{
+            const product = req.body;
+            const result = await camerasCollection.insertOne(product)
+            res.send(result)
+        })
+
+        //get products by email [my products page]
+        app.get('/myProducts', async(req, res)=>{
+            const email = req.query.email
+            const query = {sellerEmail: email}
+            const result = await camerasCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        //delete my product [myProduct page]
+        app.delete('/myProducts/:id', async(req, res)=>{
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const result = await camerasCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        //update one property [myProducts page]
+        app.put('/advertise/:id', async(req, res)=>{
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true}
+            const updatedDoc ={
+                $set: {
+                    advertise: 'add'
+                }
+            }
+            const result = await camerasCollection.updateOne(filter,updatedDoc, options )
+            res.send(result)
+        })
+
+        //get advertise product [myProduct page]
+        app.get('/advertise/:av', async(req, res)=>{
+            const av = req.params.av
+            console.log(av)
+            const query ={advertise: av}
+            const result = await camerasCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        //get all sellers and buyer [allSellers page][all buyers page]
+        app.get('/usersRole/:role', verifyJwt, async(req, res)=>{
+            const role = req.params.role
+            const query ={role: role}
+            const result = await usersCollection.find(query).toArray()
+            res.send(result)
+        })
+
+        //find adminRoute true or false
+        app.get('/users/admin/:email', async(req, res)=>{
+            const email = req.params.email
+            const query = {email}
+            const user = await usersCollection.findOne(query)
+            res.send({isAdmin: user?.role === 'admin'})
+        })
+
+        //find sellerRoute true or false
+        app.get('/users/seller/:email', async(req, res)=>{
+            const email = req.params.email
+            const query ={ email }
+            const user = await usersCollection.findOne(query)
+            res.send({isSeller: user?.role === 'seller'})
+        })
+
+        //delete seller and buyer
+        app.delete('/user/:id', async(req, res)=>{
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)}
+            const result = await usersCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        //sellerVerify
+        app.put('/user/:id', async(req, res)=>{
+            const id = req.params.id
+            const filter = {_id: ObjectId(id)}
+            const options ={upsert: true}
+            const updatedDoc ={
+                $set :{
+                    status: 'verify'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options)
+            res.send(result)
+        })
+
+        //JWT 
+        app.get('/jwt', async(req, res)=>{
+            const email = req.query.email
+            console.log(email)
+            const query = {email}
+            const user = await usersCollection.findOne(query)
+
+            if(user){
+                const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '14d'})
+                return res.send({accessToken: token})
+            }
+            res.status(403).send({accessToken: ''})
+        })
+    }
+    finally{
+
+    }
+}
+
+run()
+
+
+
+
+app.get('/', (req, res) =>{
+    res.send('server is on ..........................>>>')
+})
+
+
+app.listen(port)
